0c2df606c7a3cfb243cf30e35fd862cac6237ad3
eder-riente <diasriente@gmail.com>
2022-07-10 11:57:11 -0300

    feat: class 2 completed

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,15 +1,15 @@
 import { Header } from "../components/Header";
-import { Lesson } from "../components/Lesson";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event() {
   return (
-    <div>
+    <div className="flex flex-col">
       <Header />
-      <Sidebar />
-      <Video />
-      <Lesson />
+      <main className="flex flex-1 min-h-screen">
+        <Video />
+        <Sidebar />
+      </main>
     </div>
   );
 }
