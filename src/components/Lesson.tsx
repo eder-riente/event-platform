d1d5b9cf4b81ee912c54aaaee9cddb830e1f1073
eder-riente <diasriente@gmail.com>
2022-07-16 20:11:20 -0300

    feat: aula 03

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { Link } from "react-router-dom";
 interface LessonProps {
   title: string;
   slug: string;
@@ -15,30 +16,29 @@ export function Lesson(props: LessonProps) {
     "EEEE' • 'd' de 'MMMM' • 'k'h'mm ",
     { locale: ptBR }
   );
+
   return (
-    <div className="">
-      <a href="#">
-        <span className="text-gray-300">{availableDateFormatted}</span>
-        <div className="rounded border border-gray-500 p-4 mt-2 ">
-          <header className="flex item-center justify-between">
-            {isLessonAvailable ? (
-              <span className=" flex items-center gap-2 text-sm text-blue-500 font-medium">
-                <CheckCircle size={20} />
-                Conteudo Liberado
-              </span>
-            ) : (
-              <span className=" flex items-center gap-2 text-sm text-orange-500 font-medium">
-                <Lock size={20} />
-                Em breve
-              </span>
-            )}
-            <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
-              {props.type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
+    <Link to={`/event/lesson/${props.slug}`} className="group">
+      <span className="text-gray-300">{availableDateFormatted}</span>
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+        <header className="flex item-center justify-between">
+          {isLessonAvailable ? (
+            <span className=" flex items-center gap-2 text-sm text-blue-500 font-medium">
+              <CheckCircle size={20} />
+              Conteudo Liberado
             </span>
-          </header>
-          <strong className="text-gray-200 mt-5 block">{props.title}</strong>
-        </div>
-      </a>
-    </div>
+          ) : (
+            <span className=" flex items-center gap-2 text-sm text-orange-500 font-medium">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+          <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
+            {props.type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
+          </span>
+        </header>
+        <strong className="text-gray-200 mt-5 block">{props.title}</strong>
+      </div>
+    </Link>
   );
 }
